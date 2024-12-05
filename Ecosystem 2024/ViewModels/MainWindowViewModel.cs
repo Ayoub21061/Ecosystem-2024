c101c2ad60fc7c34582e6e940e40276d0317f764
Ayoub21061 <21061@ecam.be>
2024-12-05 14:32:47 +0100

    Herbivore displayed

--- Ecosystem 2024/ViewModels/MainWindowViewModel.cs
+++ Ecosystem 2024/ViewModels/MainWindowViewModel.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text.Json;
 using Avalonia.Markup.Xaml.MarkupExtensions;
+using System.ComponentModel;
 
 
 namespace Ecosystem_2024.ViewModels;
@@ -17,6 +18,7 @@ namespace Ecosystem_2024.ViewModels;
 public partial class MainWindowViewModel : GameBase
 {
     public Carnivore carnivore;
+    public Herbivore herbivore;
     public int Width {get;} = 800;
 
     public int Height {get;} = 450;
@@ -26,6 +28,9 @@ public partial class MainWindowViewModel : GameBase
     public MainWindowViewModel() {
         carnivore = new Carnivore(new Point(Width/2, Height/2));
         GameObjects.Add(carnivore);
+
+        herbivore = new Herbivore(new Point(Width/2+32, Height/2+32));
+        GameObjects.Add(herbivore);
     }
 
     protected override void Tick() {}
